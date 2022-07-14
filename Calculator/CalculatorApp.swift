85c92a392758c5a8fb7beb53da7f61f0d73d3022
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2022-07-14 16:50:28 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tueloper on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
